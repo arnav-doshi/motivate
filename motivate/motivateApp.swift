2a8f56e44e7b94a3fde269e095e2d35a2dcc48d5
Arnav Doshi <arnavdoshi47@gmail.com>
2021-09-20 00:28:27 -0700

    Initial Commit

---
+++ motivate/motivateApp.swift
@@ -0,0 +1,17 @@
+//
+//  motivateApp.swift
+//  motivate
+//
+//  Created by Arnav Doshi on 9/20/21.
+//
+
+import SwiftUI
+
+@main
+struct motivateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
